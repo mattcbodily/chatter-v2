74ecaf12a78186a5658b8965e97cf8d77d7a3325
mattcbodily <mattcbodily@gmail.com>
2020-07-11 00:08:05 -0600

    pushing up

--- src/Components/GroupModal/GroupModal.js
+++ src/Components/GroupModal/GroupModal.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 
 class GroupModal extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            groupName: '',
+            directMessage: false,
+            
+        }
+    }
+
     render(){
         return (
             <div>
