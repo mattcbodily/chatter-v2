2a46303900bad0afe1c73532d7614e2cc9f48178
mattcbodily <mattcbodily@gmail.com>
2020-07-17 11:44:43 -0600

    added mobile header view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,13 +3,17 @@ import {withRouter, Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import axios from 'axios';
 import SideMenu from '../SideMenu/SideMenu';
+import hamburger from '../../assets/menu.svg';
+import moreVertical from '../../assets/more-vertical.svg';
 import './Header.scss';
 
 class Header extends Component {
     constructor(props){
         super(props);
         this.state = {
-            chatGroups: []
+            chatGroups: [],
+            sideMenuView: false,
+            mainMenuView: false
         }
     }
 
@@ -27,16 +31,34 @@ class Header extends Component {
     }
 
     render(){
+        const {sideMenuView, mainMenuView} = this.state;
         return (
             <div className='main-header'>
-                <h1>Chatter</h1>
-                <nav>
-                    <Link to='/chat/0'>Dashboard</Link>
-                    <Link to='/profile'>Profile</Link>
-                </nav>
-                {this.props.location.pathname !== '/profile'
-                ? <SideMenu chatGroups={this.state.chatGroups} getGroupFn={this.getGroups}/>
-                : null}
+                <header className='header-mobile'>
+                    <img src={hamburger} alt='Menu'/>
+                    {sideMenuView
+                    ? <SideMenu />
+                    : null}
+                    <img src={moreVertical} alt='Group Menu'/>
+                    {mainMenuView
+                    ? (
+                        <nav className='dropdown'>
+                            <Link to='/chat/0'>Dashboard</Link>
+                            <Link to='/profile'>Profile</Link>
+                        </nav>
+                    )
+                    : null}
+                </header>
+                <header className='header-desktop'>
+                    <h1>Chatter</h1>
+                    <nav>
+                        <Link to='/chat/0'>Dashboard</Link>
+                        <Link to='/profile'>Profile</Link>
+                    </nav>
+                    {this.props.location.pathname !== '/profile'
+                    ? <SideMenu chatGroups={this.state.chatGroups} getGroupFn={this.getGroups}/>
+                    : null}
+                </header>
             </div>
         )
     }
