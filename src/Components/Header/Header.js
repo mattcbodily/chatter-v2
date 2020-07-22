28aa59e1fea7b8074d8e7894f77b1d4350f14354
mattcbodily <mattcbodily@gmail.com>
2020-07-22 14:49:47 -0600

    added selected group name to the header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {withRouter, Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import axios from 'axios';
+import {selectChat} from '../../redux/reducer';
 import SideMenu from '../SideMenu/SideMenu';
 import hamburger from '../../assets/menu.svg';
 import moreVertical from '../../assets/more-vertical.svg';
@@ -25,7 +26,9 @@ class Header extends Component {
         const {user} = this.props;
         axios.get(`/api/groups/${user.user_id}`)
         .then(res => {
-            this.setState({chatGroups: res.data})
+            this.setState({chatGroups: res.data});
+            this.props.history.push(`/chat/${res.data[0].group_id}`);
+            this.props.selectChat(res.data[0].group_id);
         })
         .catch(err => console.log(err));
     }
@@ -49,6 +52,7 @@ class Header extends Component {
                     {sideMenuView
                     ? <SideMenu chatGroups={chatGroups} getGroupFn={this.getGroups} toggleFn={this.toggleSideMenu}/>
                     : null}
+                    <h3 className='group-name'>{chatGroups.find(e => e.group_id === selectedChat)?.group_name}</h3>
                     <img src={moreVertical} alt='Group Menu' onClick={this.toggleMainMenu}/>
                     {mainMenuView
                     ? (
@@ -76,4 +80,4 @@ class Header extends Component {
 
 const mapStateToProps = reduxState => reduxState;
 
-export default withRouter(connect(mapStateToProps)(Header));
+export default withRouter(connect(mapStateToProps, {selectChat})(Header));
